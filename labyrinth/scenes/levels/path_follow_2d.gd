1ee27fd85a4f1422d276f3c244e96295cc493a30
Shawn Taylor <shawn@mapsandapps.net>
2024-07-30 16:33:52 -0400

    adjust acceleration & deceleration

--- labyrinth/scenes/levels/path_follow_2d.gd
+++ labyrinth/scenes/levels/path_follow_2d.gd
@@ -1,6 +1,8 @@
 extends PathFollow2D
 
 const MAX_SPEED = 100
+const ACCELERATION_RATE = 0.05
+const DECELERATION_RATE = -0.01
 var speed = 0
 var speed_delta = 0
 var is_slowing = false
@@ -18,7 +20,7 @@ func _process(delta):
 		pass
 
 func _on_area_2d_area_entered(area):
-	speed_delta = MAX_SPEED * 0.1 # accelerate
+	speed_delta = MAX_SPEED * ACCELERATION_RATE
 
 func _on_area_2d_area_exited(area):
-	speed_delta = MAX_SPEED * -0.1 # slow down
+	speed_delta = MAX_SPEED * DECELERATION_RATE
